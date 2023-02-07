eb4cc5a160027681e2647327cab1747ae8df9627
Ygor Barbosa <ygor.s.barbosa@gmail.com>
2023-02-07 11:41:57 -0300

    lampada

---
+++ intro.js
@@ -0,0 +1,9 @@
+const quebrada = false;
+function mudaLampada(tipo) {
+  if (!quebrada) {
+    document.getElementById("luz").src = "_imagens/" + tipo + ".jpg";
+    if (tipo == "lampada-quebrada") {
+      quebrada = true;
+    }
+  }
+}
